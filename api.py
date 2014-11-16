498adb628623de9e1225e288ea942224d3a3a056
Richard van der Hoff <git@rvanderhoff.org.uk>
2014-11-16 21:12:26 +0000

    more refactoring

--- api.py
+++ api.py
@@ -3,12 +3,15 @@
 import optparse
 import os
 import select
-from struct import pack,unpack
+from struct import calcsize,pack,unpack
 import sys
 import termios
 import time
 
 class XBeeController(object):
+    AT_COMMAND=0x08
+    AT_COMMAND_RESPONSE = 0x88
+
     def __init__(self, dev="/dev/ttyS1"):
         self.fh=open('/dev/ttyS1','rw+',0)
         self.configure()
@@ -50,15 +53,17 @@ class XBeeController(object):
             raise Exception("mismatched checksum")
         return frame
 
-    def send_frame(self, api_identifier, frame_no, api_frame):
+    def send_frame(self, api_identifier, api_frame):
         """
         send an API frame
 
         api_identifier: integer api identifier
-        frame_no: integer frame number
         api_frame: list of bytes
+
+        returns: frame number
         """
 
+        frame_no = 1
         frame = [api_identifier, frame_no] + [b for b in api_frame]
         checksum = 0xFF - (sum(frame) & 0xFF)
         len_bytes = pack(">BH",0x7E,len(frame))
@@ -71,53 +76,68 @@ class XBeeController(object):
         print
 
         self.fh.write(string)
+        return frame_no
 
-    def api_command(self, api_identifier, api_frame):
-        """
-        send an api frame, and await the response
+    def at_command(self, cmd, multi_response=False):
+        """send an AT command using the API, and return the result
 
-        api_identifier: integer api identifier
-        api_frame: list of bytes
+        cmd: 2-letter AT command
 
-        returns tuple (response_type,[result_bytes])
-        """
-        frameno = 1
-        self.send_frame(api_identifier, frameno, api_frame)
-        resp=self.read_frame()
-        
-        if resp[1] != frameno:
-            raise Exception("Unexpected seqno %02x" % resp[1])
+        multi_response: True if command results in multiple responses. In this
+          case, response will be a list
 
-        return (resp[0], resp[2:])
+        returns: result bytes
 
-    def at_command(self, cmd):
-        """
-        send an AT command using the API, and return the result
-
-        returns: integer result, or None on error
         """
 
         print ">> %s" % cmd
-        (rt, rb) = self.api_command(0x08,[ord(cmd[0]),ord(cmd[1])])
+        api_frame = [ord(cmd[0]),ord(cmd[1])]
+        frame_no = self.send_frame(self.AT_COMMAND, api_frame)
 
-        if rt != 0x88:
-            raise Exception("Unexpected response type %02x" % rt)
+        results = []
+        while True:
+            resp=self.read_frame()
 
-        if rb[2] != 0:
-            print "<< ERROR"
-            return None
+            if resp[1] != frame_no:
+                raise Exception("Unexpected seqno %02x" % resp[1])
 
-        s = 0
-        for b in rb[3:]:
-            s <<= 8
-            s |= b
+            if resp[0] != self.AT_COMMAND_RESPONSE:
+                raise Exception("Unexpected response type %02x" % rt)
 
-        print ">> OK %i" % s
-        return s
+            if resp[4] != 0:
+                print "<< ERROR %i" % resp[4]
+                raise Exception("Error from API command")
+
+            result = resp[5:]
+            print "<< OK %s" % (" ".join(("%02x" % b for b in result)))
+
+            if not multi_response:
+                return result
+
+            if len(result) == 0:
+                return results
+
+            results.append(result)
+                
+            if multi_response and len(result) == 0:
+                return results
 
 
 def writer(ctl):
-    r = ctl.at_command("DL")
+    r = ctl.at_command("ND", True)
+    for node in r:
+        nodestr = "".join((chr(b) for b in node))
+        fmt = ">HLLB"
+        fmtlen = calcsize(fmt)
+        (my,sh,sl,db) = unpack(fmt,nodestr[0:fmtlen])
+        ni = nodestr[fmtlen:-1]
+
+        print "MY: %04x" % my
+        print "SH: %08x" % sh
+        print "SL: %08x" % sl
+        print "DB: %02x" % db
+        print "NI: %s" % ni
+        print
 
 def await_at_response(fh, timeout=3):
     buf=""
